708c0c3dc9ebc544e6d2fe3dbcdfc6c1e9bc1c06
Julia Costa <juliacosta232005@gmail.com>
2024-04-26 10:22:35 +0100

    Add constructor to class Player

--- PlayerManager1/Player.cs
+++ PlayerManager1/Player.cs
@@ -6,5 +6,10 @@ namespace PlayerManager1
     {
         public string Name { get; }
         public int Score { get; private set; }
+        public Player(string _name, int _score)
+        {
+            Name = _name;
+            Score = _score;
+        }
     }
 }
