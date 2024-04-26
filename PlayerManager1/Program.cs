2112a4cd4138f2d87db00c3f1e26e8bad78cf9b5
Julia Costa <juliacosta232005@gmail.com>
2024-04-26 10:30:12 +0100

    Add InsertPlayer() method to class Program

--- PlayerManager1/Program.cs
+++ PlayerManager1/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PlayerManager1
 {
     class Program
     {
+        private List<Player> _playerList;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+        }
+        private void InsertPlayer()
+        {
+            Console.Write("Insert player name:\n> ");
+            string _name = Console.ReadLine();
+
+            Console.Write("\nInsert player score:\n> ");
+            int _score = int.Parse(Console.ReadLine());
+            
+            Player newPlayer = new Player(_name,_score);
+            _playerList.Add(newPlayer);
         }
     }
 }
